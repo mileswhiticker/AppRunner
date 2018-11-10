53580c96849f9fa5c93cfc80bff9b22e221ce82b
Terry Franklin <terryfranklin82@gmail.com>
2018-11-10 14:59:19 +1000

    scrolling grass

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -7,13 +7,18 @@ public class PlayerMovement : MonoBehaviour {
     public GroundMovement gm;
     private Rigidbody rigidbody;
     public float speed = 10f;
-    Renderer renderer;
-    public float scrollSpeed = 0.5f;
+    public Renderer groundRenderer;
+    public float scrollSpeed = 0.008f;
+    public Material myMaterial;
+    public Vector2 textureOffset;
+    public List<FlyingApp> allFlyingApps = new List<FlyingApp>();
+    public GameObject FlyingAppPrefab;
 
     // Use this for initialization
     void Start () {
         rigidbody = GetComponent<Rigidbody>();
-        renderer = gm.GetComponent<Renderer>();
+        groundRenderer = gm.GetComponent<Renderer>();
+        textureOffset = new Vector2(speed, 0);
 	}
 	
 	// Update is called once per frame
@@ -21,9 +26,27 @@ public class PlayerMovement : MonoBehaviour {
         float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 
         Vector3 movement = new Vector3(horizontalMovement, 0.0f, 0.0f);
-        renderer.material.shader.off
+        //renderer.material.shader.off
+
+        textureOffset.x += scrollSpeed; 
+
+        //Shader shader = gm.GetComponent<Shader>();
+        myMaterial = groundRenderer.material;
+        myMaterial.SetTextureOffset("_MainTex", textureOffset);
+
+        //Debug.Log("test");
+
+        //Texture myTex = material.mainTexture;
+        //myTex.SetTextureOffset("_MainTex",textureOffset);
 
         rigidbody.AddForce(movement * speed);
 
     }
+
+    void CreateApp()
+    {
+        Instantiate(FlyingAppPrefab);
+    }
+
+
 }
