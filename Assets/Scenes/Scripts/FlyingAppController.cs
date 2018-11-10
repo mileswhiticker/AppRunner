140495d16022b57e058959c5b4163ea1b8ebca9f
Terry Franklin <terryfranklin82@gmail.com>
2018-11-11 09:38:47 +1000

    flying apps now move towards player

--- Assets/Scenes/Scripts/FlyingAppController.cs
+++ Assets/Scenes/Scripts/FlyingAppController.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class FlyingAppController : MonoBehaviour {
 
 	public GameObject flyingApp;
-	public List<GameObject> allFlyingApps = new List<GameObject()>();
+	public List<GameObject> flyingApps = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		// Launch an app 3 seconds after starting, then every 3 seconds after that
-		InvokeRepeating("CreateApp", 3.0f, 3.0f);
+		// Launch an app 1 seconds after starting, then every 3 seconds after that
+		InvokeRepeating("CreateApp", 1.0f, 3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Move all flying apps in the list closer to the player
+		foreach (GameObject flyingApp in flyingApps)
+		{
+			flyingApp.transform.position = new Vector3(flyingApp.transform.position.x, flyingApp.transform.position.y, flyingApp.transform.position.z - Time.deltaTime * 1.5f);
+		}
 	}
-	
+
+	// Spawn a new flying app and add it to the list
 	void CreateApp()
     {
-		Debug.Log("Launch an app");
-		flyingApp = Instantiate(Resources.Load("FlyingAppPrefab")) as GameObject;
-		allFlyingApps.Add(flyingApp);
+		flyingApp = Instantiate(Resources.Load("flyingAppPrefab")) as GameObject;
+		flyingApp.transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 0.85f, 3f);
+		flyingApps.Add(flyingApp);
     }
 }
