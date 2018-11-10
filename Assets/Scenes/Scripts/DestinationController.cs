fafbb3dd8781e2ed3e4543e17ac47cc953f61323
Miles Whiticker <miles.whiticker@gmail.com>
2018-11-10 17:09:27 +1000

    add placeholder system for destinations
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

---
+++ Assets/Scenes/Scripts/DestinationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationController : MonoBehaviour {
+    public GameObject PlayerGameObject;
+    private PlayerMovement PlayerMovementScript;
+    public float DistanceBetweenDestinations = 15.0f;
+    public float DistanceAtLastDestination = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayerMovementScript = PlayerGameObject.GetComponent<PlayerMovement>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(PlayerMovementScript.DistanceTravelled > DistanceAtLastDestination + DistanceBetweenDestinations)
+        {
+            //
+        }
+	}
+}
--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public partial class PlayerMovement : MonoBehaviour {
 
     public GameObject GroundPlane;
     private Renderer groundRenderer;
-    public float strafeSpeed = 0.5f;
+    public float strafeSpeed = 10.0f;
     public float grassScrollSpeed = 0.008f;
     public Material myMaterial;
     public Vector2 textureOffset = new Vector2(0, 0);
     public List<FlyingApp> allFlyingApps = new List<FlyingApp>();
     public GameObject FlyingAppPrefab;
-    public float MaxWidth = 9.0f;
+    public float MaxStrafeWidth = 9.0f;
+    public float MaxStrafeWidthNormal = 9.0f;
+    public float MaxStrafeWidthDestination = 4.0f;
     public float DistanceTravelled = 0.0f;
     public float PlayerRunRate = 1.0f;
+    public float PlayerRunRateMax = 2.0f;
+    public float PlayerRunRateMin = 0.5f;
+    public bool AtDestination = false;
 
     // Use this for initialization
     void Start ()
@@ -27,29 +32,38 @@ public class PlayerMovement : MonoBehaviour {
     {
         //strafing
         float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * strafeSpeed;
-        
+        Debug.Log("horizontalMovement: " + horizontalMovement);
+
         //clamp the value
-        if (this.transform.position.x <= -MaxWidth)
+        if (this.transform.position.x <= -MaxStrafeWidth)
         {
             horizontalMovement = Mathf.Max(horizontalMovement, 0);
         }
-        if (this.transform.position.x >= MaxWidth)
+        if (this.transform.position.x >= MaxStrafeWidth)
         {
             horizontalMovement = Mathf.Min(horizontalMovement, 0);
         }
 
-        //set it 
+        //set it
+        this.transform.Translate(horizontalMovement, 0, 0);
+
+        //running forward
+        if (AtDestination)
         {
-            this.transform.Translate(horizontalMovement, 0, 0);
+            if(Input.GetAxis("Vertical") > 0)
+            {
+                LeaveDestination();
+            }
+        }
+        else
+        {
+            textureOffset.y -= grassScrollSpeed * PlayerRunRate;
+            myMaterial = groundRenderer.material;
+            myMaterial.SetTextureOffset("_MainTex", textureOffset);
         }
         
-        //running forward
-        textureOffset.y -= grassScrollSpeed * PlayerRunRate;
-        myMaterial = groundRenderer.material;
-        myMaterial.SetTextureOffset("_MainTex", textureOffset);
-        
+        //track distance travelled
         DistanceTravelled += PlayerRunRate * Time.deltaTime;
-
     }
 
     void CreateApp()
