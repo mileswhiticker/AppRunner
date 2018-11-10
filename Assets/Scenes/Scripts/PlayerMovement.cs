90f41cdd7010874c9d136870ffc25c0910fe5e43
CA <cael_aislinn@yahooc.com.au>
2018-11-10 12:37:16 +1000

    initial commit project files
    Signed-off-by: gamesfreak26 <gamesfreak26@gmail.com>

---
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GroundMovement gm;
+    private Rigidbody rigidbody;
+    public float speed = 10f;
+    Renderer renderer;
+    public float scrollSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        renderer = gm.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, 0.0f);
+        renderer.material.shader.off
+
+        rigidbody.AddForce(movement * speed);
+
+    }
+}
