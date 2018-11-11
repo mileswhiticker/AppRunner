f48ddabc357271252e4e7104cc2206344fce3361
Miles Whiticker <miles.whiticker@gmail.com>
2018-11-11 10:35:17 +1000

    DestinationController is unused
    Signed-off-by: Miles Whiticker <miles.whiticker@gmail.com>

--- Assets/Scenes/Scripts/DestinationController.cs
+++ Assets/Scenes/Scripts/DestinationController.cs
@@ -7,19 +7,29 @@ public class DestinationController : MonoBehaviour {
     private PlayerMovement PlayerMovementScript;
     public float DistanceBetweenDestinations = 15.0f;
     public float DistanceAtLastDestination = 0.0f;
+    public GameObject DestinationHome;
+    public GameObject DestinationWork;
+    public GameObject DestinationSocial;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
+        /*
         PlayerMovementScript = PlayerGameObject.GetComponent<PlayerMovement>();
+        Destination workDestination = DestinationWork.GetComponent<Destination>();
+        Destination socialDestination = DestinationSocial.GetComponent<Destination>();
+        Destination homeDestination = DestinationHome.GetComponent<Destination>();
+        */
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update ()
     {
+        /*
 		if(PlayerMovementScript.DistanceTravelled > DistanceAtLastDestination + DistanceBetweenDestinations)
         {
             //
         }
+        */
 	}
 }
