7dc451d2dd82a7dce4d00faab6c6464864d4f639
Terry Franklin <terryfranklin82@gmail.com>
2018-11-11 14:10:56 +1000

    list of prefabs added

--- Assets/Scenes/Scripts/FlyingAppController.cs
+++ Assets/Scenes/Scripts/FlyingAppController.cs
@@ -45,6 +45,25 @@ public class FlyingAppController : MonoBehaviour {
 	// Spawn a new flying app and add it to the list
 	void CreateApp()
     {
+		List<String> apps = [
+			"AppAirbnb",
+			"AppAirbnb2",
+			"AppAirtasker",
+			"AppAirtasker2",
+			"AppAmazon",
+			"AppCandycrush",
+			"AppEbay",
+			"AppFacebook",
+			"AppFiverr",
+			"AppFiverr2",
+			"AppGithub",
+			"AppGoogledocs",
+			"AppInstagram",
+			"AppMeditation",
+			"AppMicrosoft",
+			"AppMinecraft"
+		];
+		
 		flyingApp = Instantiate(Resources.Load("flyingAppPrefab")) as GameObject;
 		// Material flyingAppMaterial = flyingApp.GetComponent<Renderer>().AddComponent(typeof(Material)) as Material;
 		// Material flyingAppMaterial = flyingApp.GetComponent<Material>();
